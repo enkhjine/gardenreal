1a38494a0d68bf8a7451777cd8242752bcac3a6d
tdaa95 <javkhlantur.g@gmail.com>
2017-01-24 00:44:24 +0800

    baasiin tohirgoo hiisen

--- BusinessLogic/ejbModule/logic/SuperBusinessLogic.java
+++ BusinessLogic/ejbModule/logic/SuperBusinessLogic.java
@@ -30,7 +30,7 @@ import javax.persistence.criteria.Root;
 
 public class SuperBusinessLogic {
 
-	private String applicationName = "Hospital";
+	private String applicationName = "Garden";
 	private EntityManager entityManager;
 	
 	public EntityManager entityManager() throws Exception{
